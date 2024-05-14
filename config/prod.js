39776229ae99436d6b8f98354bd96512b8e8b514
Yufei Wang <ywang447@usc.edu>
2024-05-14 11:05:07 -0700

    Delete config/prod.js

--- config/prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MongoDB: process.env.MONGO_DB,
-    FacebookAppID: '1239705337415895',
-    FacebookAppSecret: '5a073be492f3c167c160778ed8e45bfd',
-    AWSAcessKeyID: process.env.AWS_ACESS_KEY,
-    AWSAcessKeySecret: process.env.AWS_ACESS_SECRET
-};
